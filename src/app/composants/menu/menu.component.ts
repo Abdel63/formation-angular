1c03f01573564dab24519371c5f5f936ec2e11c6
Abdel63 <abdel6391@gmail.com>
2022-07-08 16:49:49 +0200

    connection deconnection crud

--- src/app/composants/menu/menu.component.ts
+++ src/app/composants/menu/menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Stagiaire } from 'src/app/interfaces/stagiaire';
+import { Utilisateur } from 'src/app/interfaces/utilisateur';
+import { UtilisateurService } from 'src/app/services/utilisateur.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,19 +9,35 @@ import { Stagiaire } from 'src/app/interfaces/stagiaire';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-  title = 'formation-angular';
-  tab: number[] = [2, 5, 3, 8];
-  moyennes: number[] = [12, 5, 16, 18];
-  stagiaire: Stagiaire = {num: 100, nom: "wick", prenom: "john"}
-  stagiaires : Stagiaire[] = [
-    { num: 100, nom: "wick", prenom: "john" },
-    { num: 200, nom: "dalton", prenom: "jack" },
-    { num: 300, nom: "doe", prenom: "joe" },
-    { num: 400, nom: "maggio", prenom: "sophie" },
-  ]
-  constructor() { }
+  // title = 'formation-angular';
+  // tab: number[] = [2, 5, 3, 8];
+  // moyennes: number[] = [12, 5, 16, 18];
+  // stagiaire: Stagiaire = {num: 100, nom: "wick", prenom: "john"}
+  // stagiaires : Stagiaire[] = [
+  //   { num: 100, nom: "wick", prenom: "john" },
+  //   { num: 200, nom: "dalton", prenom: "jack" },
+  //   { num: 300, nom: "doe", prenom: "joe" },
+  //   { num: 400, nom: "maggio", prenom: "sophie" },
+  // ]
+  utilisateur: Utilisateur = {};
+  isConnected = false;
+  constructor(private us: UtilisateurService) { }
 
   ngOnInit(): void {
   }
-
+  seConnecter() {
+    this.us.checkUser(this.utilisateur).subscribe(res => {
+      if (res.length > 0) {
+        this.isConnected = true;
+        const userString = JSON.stringify(res[0]);  // j'ai transformé l'objet en string
+        this.utilisateur = res[0];
+        localStorage.setItem('user', userString) // on ajoute un item dans la mémoire qu'on va appeler user
+      
+      }
+    })
+  }
+  seDeconnecter() {
+      this.isConnected = false;
+      localStorage.removeItem('user'); // supprimer l'utilisateur quand il se déconnecte
+  }
 }
